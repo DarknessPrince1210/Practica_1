5775ff703e34a2feb5db7acd60a9dac2d446fbf9
DarknessPrince1210 <diaz.tiburcio.ismael@gmail.com>
2022-09-24 13:28:35 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+
+
 const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const inputOriginal = document.getElementById('input-original');
 const cifrador = document.getElementById('cifrador');
